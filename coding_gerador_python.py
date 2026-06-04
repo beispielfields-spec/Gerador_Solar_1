0fafb35233f8dfe4b8c6c338e3644f4d08a5ec7f
beispielfields-spec <beispielfields@gmail.com>
2026-06-04 12:27:27 -0300

    Update coding_gerador_python.py

--- coding_gerador_python.py
+++ coding_gerador_python.py
@@ -22,10 +22,10 @@ with st.sidebar:
     # Define o título da barra lateral
     st.title("🤖 DADOS DE CONSUMO")
   # Campo para inserir o consumo mensal
-    CONSUMO_MENSAL = st.float(input(f"  Mês {mes:02d}: "))
-        "Insira consumo menal", 
-        type="float",
-        help="Obtenha os dados do projeto"
+  CONSUMO_MENSAL = st.float(input(f"  Mês {mes:02d}: "))
+      "Insira consumo menal", 
+      type="float",
+      help="Obtenha os dados do projeto"
   )
 
 # --------------------------------------------------
